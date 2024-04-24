e09bf279e74dee0d340b82ab1907ac7451ca5020
michaeldepp <michaeldepp7@gmail.com>
2024-04-24 13:32:48 +0800

    feat : add the redux saga toolkits

---
+++ src/redux/sagas/index.js
@@ -0,0 +1,10 @@
+import { all } from 'redux-saga/effects';
+
+// Import and combine your sagas here
+function* rootSaga() {
+	yield all([
+		// Add your sagas here
+	]);
+}
+
+export default rootSaga;
